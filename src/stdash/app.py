9b5061bef8188ba91b17495e5cbd8a7e6f6f7436
Nicou11 <lyoungin1204@gmail.com>
2024-09-27 12:34:49 +0900

    add legend

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 
-st.title('CNN JOB MON')
+st.title('요청 / 처리 건수(h)')
 
 def load_data():
     url = 'http://43.202.66.118:8006/all'
@@ -18,15 +18,24 @@ df = pd.DataFrame(data)
 # request_time, prediction_time 이용해서 '%Y-%m-%d %H' 형식
 # 즉 시간별로 GROUP BY COUNT 하여 plt 차트 그려보기
 
-df['request_time'] = pd.to_datetime(df['request_time'])
-df['request_time'] = df['request_time'].dt.strftime('%Y-%m-%d %H')
-time = df.groupby('request_time').size()
+font1 = {'family': 'serif',
+      'color':  'darkred',
+      'weight': 'normal',
+      'size': 16}
 
-plt.bar(time.index, time.values)
-plt.plot(time.index, time.values, marker='o', color='r')
-plt.xlabel('Request Time')
+df['request_time'] = pd.to_datetime(df['request_time']).dt.strftime('%Y-%m-%d %H')
+df['prediction_time'] = pd.to_datetime(df['prediction_time']).dt.strftime('%Y-%m-%d %H')
+req_time = df.groupby('request_time').size()
+pre_time = df.groupby('prediction_time').size()
+
+plt.title("Requests by Date and Time")
+#plt.text(3.5, 220, 'High', fontdict=font1)
+plt.bar(req_time.index, req_time.values, label='Request')
+plt.plot(pre_time.index, pre_time.values, marker='o', color='r', label='Prediction')
+plt.xlabel('Date, Time')
 plt.ylabel('Count')
 plt.xticks(rotation = 45)
+plt.legend()
 
 # 화면에 그리기
 st.pyplot(plt)
